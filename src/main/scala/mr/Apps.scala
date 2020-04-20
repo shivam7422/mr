6f975dee8533597d49a5fbdda5dd393baaa482a1
shivam7422 <63632045+shivam7422@users.noreply.github.com>
2020-04-20 14:38:16 +0530

    code changes

--- src/main/scala/mr/Apps.scala
+++ src/main/scala/mr/Apps.scala
@@ -5,25 +5,7 @@ class Ram
   val s= " shivam "
   println(" My name is " +s)
 }
-class Shyam extends Ram
-{
-
-  def details: Unit =
-  {
-    val bank= " State bank of india"
-    println(" My bank name is " +bank)
-
-  }
-}
-class  mild extends Shyam
-{
-  val e= "Kadipur"
-  println(" My hometown is " + e)
-}
 object Apps extends App{
 
- val e= new mild
-  e.details
-
-
+ val e= new Ram
 }
